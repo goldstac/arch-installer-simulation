011880d295a72a0d43c51675ba74998cd316cf57
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-06-29 23:05:55 +0400

    add clear to shell

--- src/core/main.py
+++ src/core/main.py
@@ -1,5 +1,11 @@
+import os
 import time
 while True:
  main_terminal_shell = input("root@archiso ~ # ")
  if main_terminal_shell == "exit":
   break
+ elif main_terminal_shell == "clear":
+  if os.name == "nt":
+   os.system("cls")
+  else:
+   os.system("clear")
