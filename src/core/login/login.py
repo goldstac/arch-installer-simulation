9026255b637fdbff45b1c087d3c1ed1d39e7ae7f
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-07-01 15:20:34 +0400

    change it from login.py to login.go

--- src/core/login/login.py
+++
@@ -1,8 +0,0 @@
-print("Welcome To Arch Linux")
-username = "admin"
-password = "iusearchbtw"
-ask_username = input("Enter Username : ")
-if username == {username}:
-    print("Username Correct")
-    ask_passwd = input ("Enter Your Password")
-    i
